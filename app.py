3f8d86f03337a75af430a0be4307300f713d15a6
Vinoth-24 <91379271+Vinoth-24@users.noreply.github.com>
2022-01-30 17:33:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def predict():
         my_prediction = prediction(processed)
         return render_template('result.html', prediction=my_prediction)
 
-port = int(os.environ.get('PORT', 5000))
+port = int(os.environ.get('PORT', 33507))
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=port, debug=True)
